675d1c77dabdcfb71c2687468a290d54977a2312
Aika Sat <aikasat95@gmail.com>
2019-12-27 18:31:31 +0600

    -wrapped content into container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React, {Fragment} from 'react';
 import Navigation from "../UI/Navigation/Navigation";
+import {Container} from "reactstrap";
 
 
 const Layout = (props) => {
   return (
       <Fragment>
+        <Container>
           <Navigation/>
           <div> {props.children}</div>
+        </Container>
+
       </Fragment>
   );
 };
